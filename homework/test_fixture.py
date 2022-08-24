4a63762567983d2e6843b9552e39abd4a3471178
satrn8 <verypyc@gmail.com>
2022-08-24 22:49:46 +0300

    commit 08/24

--- homework/test_fixture.py
+++ homework/test_fixture.py
@@ -1,11 +1,31 @@
+import pytest
+from selene.support.shared import browser
+
 """
 Сделайте разные фикстуры для каждого теста
 """
 
 
-def test_github_desktop():
-    pass
+@pytest.fixture(scope='function')
+def github_desktop():
+    browser.config.browser_name = "chrome"
+    browser.config.window_width = 1920
+    browser.config.window_height = 1024
 
 
-def test_github_mobile():
-    pass
+@pytest.fixture(scope='function')
+def github_mobile():
+    browser.config.browser_name = "chrome"
+    browser.config.window_width = 414
+    browser.config.window_height = 896
+
+
+def test_desktop(github_desktop):
+    browser.open("https://github.com/")
+    browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
+
+
+def test_mobile(github_mobile):
+    browser.open("https://github.com/")
+    browser.element('[class="octicon octicon-three-bars"]').click()
+    browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
