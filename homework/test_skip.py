e4b352baae3b525196b1a79864a43fbef3bc6e01
satrn8 <verypyc@gmail.com>
2022-09-03 18:04:19 +0300

    commit 09/03

--- homework/test_skip.py
+++ homework/test_skip.py
@@ -1,17 +1,32 @@
-"""
-Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
-"""
 import pytest
+from selene.support.shared import browser
 
 
-@pytest.fixture()
-def browser():
-    pass
+url = "https://github.com/"
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.fixture(params = [(1024, 780), (414, 896)])
+def browser_window(request):
+    return request
 
 
-def test_github_mobile(browser):
-    pass
+@pytest.fixture(scope="function", autouse=True)
+def browser_config(browser_window):
+    browser.open(url)
+    browser.config.window_height = browser_window.param[0]
+    browser.config.window_width = browser_window.param[1]
+
+
+def test_github_desktop(browser_window):
+    if browser.driver.get_window_size()["width"] < 1010:
+        pytest.skip('Size for mobile version')
+    browser.open(url)
+    browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
+
+
+def test_github_mobile():
+    if browser.driver.get_window_size()["width"] > 1011:
+        pytest.skip('Size for desktop version')
+    browser.open(url)
+    browser.element('[class="octicon octicon-three-bars"]').click()
+    browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
