4f0a1a01e1812e6004080f201ae028a1807a2c7d
satrn8 <verypyc@gmail.com>
2022-10-06 01:15:39 +0300

    commit 10/06

--- homework/test_skip.py
+++ homework/test_skip.py
@@ -13,21 +13,21 @@ def browser_window(request):
 @pytest.fixture(scope="function", autouse=True)
 def browser_config(browser_window):
     browser.open(url)
-    browser.config.window_height = browser_window.param[0]
-    browser.config.window_width = browser_window.param[1]
+    width = browser_window.param[0]
+    height = browser_window.param[1]
+    browser.driver.set_window_size(width=width, height=height)
 
 
 def test_desktop(browser_window):
     if browser.driver.get_window_size()["width"] < 1010:
         pytest.skip('Size for mobile version')
     browser.open(url)
-    browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
+    browser.element('.btn-mktg').click()
 
 
 def test_mobile():
     if browser.driver.get_window_size()["width"] > 1011:
         pytest.skip('Size for desktop version')
     browser.open(url)
-    browser.element('[class="octicon octicon-three-bars"]').click()
-    browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
+    browser.element('.btn-mktg').click()
 
